9a6ef05dc34fdd5c411b92c383d9f7223eb6e8b4
Irfan Khan <89332828+javed-mohd@users.noreply.github.com>
2024-10-22 16:32:19 +0000

    variables

---
+++ basics/variables.js
@@ -0,0 +1,11 @@
+const accountId = 123;
+let accountEmail = "irfan@gmail.com";
+var accountCity = "jaipur"
+accountPassword = 1234;
+
+// accountId = 12345
+accountEmail = "irfan.khan@gmail.com";
+accountCity = "Benguluru"
+accountPassword = 768675;
+
+console.table([accountId, accountEmail, accountCity, accountPassword]);
