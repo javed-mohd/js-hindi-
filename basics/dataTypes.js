dca508f906059fda9140f068d142b1fb1a4708d8
Irfan Khan <89332828+javed-mohd@users.noreply.github.com>
2024-11-10 08:54:09 +0000

    datatypes

---
+++ basics/dataTypes.js
@@ -0,0 +1,11 @@
+// # primitive datay type (string, number, boolean, null, undefined, symbol, BigInt)
+
+// non-primitive (Array, object, function)
+
+
+const ID = Symbol('100');
+const anotherID = Symbol('100') // work to make uneaque
+
+console.log(ID === anotherID); // false
+
+
